202f23f9120e095b3b062c4ac3ea233ee98ca5d6
Matt Umut Ucak <umutucak79@gmail.com>
2021-10-06 17:42:13 +0200

    HeroMovementScript has been reorganized and decomposed into smaller functions.

--- Assets/Scripts/HeroMovementScript.cs
+++ Assets/Scripts/HeroMovementScript.cs
@@ -7,7 +7,7 @@ public class HeroMovementScript : MonoBehaviour
     private Rigidbody2D rigidBody;
     private BoxCollider2D boxCollider;
 
-    private float speed = 2;
+    private float playerSpeed = 2;
     private float camHeight, camWidth, hitboxHeight, hitboxWidth;
     
     // Start is called before the first frame update
@@ -17,12 +17,7 @@ public class HeroMovementScript : MonoBehaviour
 
         boxCollider = GetComponent<BoxCollider2D>();
 
-        hitboxHeight = boxCollider.size.x / 2; //length from the center to the edge
-
-        Camera camera = Camera.main;
-
-        camHeight = camera.orthographicSize - hitboxHeight; //length from the center to the edge
-        camWidth = camHeight * camera.aspect;
+        InitializeBounds(boxCollider);
     }
 
     // Update is called once per frame
@@ -33,25 +28,45 @@ public class HeroMovementScript : MonoBehaviour
 
         Vector3 movement = new Vector3(x, y, 0); //The movement vector
 
-        if (movement.x > 0) //Checks if player is moving right.
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (movement.x < 0) //Checks if player is moving left.
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
+        PlayerRotate(movement);
 
-        if (transform.position.y < camHeight && //Checks if the player is in the camera.
-            transform.position.y > -camHeight && 
-            transform.position.x < camWidth  &&
-            transform.position.x > -camWidth)
+        if (InCamera(camHeight, camWidth))
         {
-            transform.Translate((movement * speed) * Time.deltaTime); //Moves the player.
+            MoveAgent(movement);
         }
         else
         {
-            Vector3 wallVector;
+            WallPush(camHeight, camWidth);
+        }
+    }
+
+    public void InitializeBounds(BoxCollider2D boxCollider)
+    {
+        Camera camera = Camera.main;
+
+        hitboxHeight = boxCollider.size.x / 2; //length from the center to the edge
+
+        camHeight = camera.orthographicSize - hitboxHeight; //length from the center to the edge
+        camWidth = camHeight * camera.aspect;
+    }
+
+    public bool InCamera(float camHeight, float camWidth)
+    {
+        return (transform.position.y < camHeight &&
+        transform.position.y > -camHeight &&
+        transform.position.x < camWidth &&
+        transform.position.x > -camWidth);
+    }
+
+    public void MoveAgent(Vector3 movement)
+    {
+        transform.Translate((movement * playerSpeed) * Time.deltaTime);
+    }
+
+    public void WallPush(float camHeight, float camWidth)
+    {
+        Vector3 wallVector;
+
             if (transform.position.y > camHeight || transform.position.y < -camHeight)
             {
                 wallVector = new Vector3(0, 0 - transform.position.y, 0); //Pushes the player from the upper and lower walls
@@ -61,7 +76,18 @@ public class HeroMovementScript : MonoBehaviour
                 wallVector = new Vector3(0 - transform.position.x, 0, 0); //Pushes the player from the left and right walls
             }
 
-            transform.Translate(wallVector * Time.deltaTime); //Moves the player
+            transform.Translate(wallVector * Time.deltaTime);
+    }
+
+    public void PlayerRotate(Vector3 movement)
+    {
+        if (movement.x > 0) //Checks if player is moving right.
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (movement.x < 0) //Checks if player is moving left.
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
         }
     }
 }
