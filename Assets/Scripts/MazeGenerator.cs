f4ed2ffab897452e0e30079aeb1450193bea2db7
Matt Umut Ucak <umutucak79@gmail.com>
2021-11-24 14:46:09 +0100

    DFS fully implemented.

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 
 public class MazeGenerator : MonoBehaviour
 {
+    public struct Position
+    {
+        public int X;
+        public int Y;
+    }
+
+    public struct Neighbor
+    {
+        public Position Position;
+        public WallState SharedWall;
+    }
+
     [Flags] public enum WallState
     {
         LEFT = 1,   //0001
@@ -14,10 +26,91 @@ public class MazeGenerator : MonoBehaviour
         VISITED = 128
     }
 
-    public static WallState[,] DFS(WallState[,] maze)
+    public static WallState[,] DFS(WallState[,] maze, int width, int height)
     {
-        Stack<WallState[,]> visited = new Stack<WallState[,]>();
+        Stack<Position> toVisit = new Stack<Position>();
+        System.Random random = new System.Random();
+
+        Position position = new Position {X = random.Next(0, width), Y = random.Next(0, height)};
+
+        maze[position.X, position.Y] |= WallState.VISITED;
+        toVisit.Push(position);
+
+        while(toVisit.Count > 0)
+        {
+            Position currentCell = toVisit.Pop();
+            List<Neighbor> neighbors = GetUnvisited(currentCell, maze, width, height);
+
+            if (neighbors.Count > 0)
+            {
+                toVisit.Push(currentCell);
+
+                int randomNeighborIndex = random.Next(0,neighbors.Count);
+                Neighbor randomNeighbor = neighbors[randomNeighborIndex];
+
+                Position randomNeighborPosition = randomNeighbor.Position;
+                maze[currentCell.X, currentCell.Y] &= ~randomNeighbor.SharedWall;
+                maze[randomNeighborPosition.X, randomNeighborPosition.Y] &= ~GetOppositeWall(randomNeighbor.SharedWall);
+                maze[randomNeighborPosition.X, randomNeighborPosition.Y] |= WallState.VISITED;
+
+                toVisit.Push(randomNeighborPosition);
+            }
+        }
+
         return maze;
+    }
+
+    public static WallState GetOppositeWall(WallState wall)
+    {
+        switch (wall)
+        {
+            case WallState.RIGHT:
+                return WallState.LEFT;
+            case WallState.LEFT:
+                return WallState.RIGHT;
+            case WallState.UP:
+                return WallState.DOWN;
+            case WallState.DOWN:
+                return WallState.UP;
+            default:
+                return WallState.LEFT; //doesn't matter
+        }
+    }
+
+    public static List<Neighbor> GetUnvisited(Position position, WallState[,] maze, int width, int height)
+    {        
+        List<Neighbor> neighbors = new List<Neighbor>();
+
+        if (position.X > 0) //left
+        {
+            if (!maze[position.X - 1, position.Y].HasFlag(WallState.VISITED))
+            {
+                neighbors.Add(new Neighbor{Position = new Position{X = position.X - 1, Y = position.Y}, SharedWall = WallState.LEFT});
+            }
+        }
+        if (position.Y > 0) //down
+        {
+            if (!maze[position.X, position.Y - 1].HasFlag(WallState.VISITED))
+            {
+                neighbors.Add(new Neighbor{Position = new Position{X = position.X, Y = position.Y - 1}, SharedWall = WallState.DOWN});
+            }
+        }
+        if (position.Y < height - 1) //up
+        {
+            if (!maze[position.X, position.Y + 1].HasFlag(WallState.VISITED))
+            {
+                neighbors.Add(new Neighbor{Position = new Position{X = position.X, Y = position.Y + 1}, SharedWall = WallState.UP});
+            }
+        }
+        if (position.X < width - 1) //right
+        {
+            if (!maze[position.X + 1, position.Y].HasFlag(WallState.VISITED))
+            {
+                neighbors.Add(new Neighbor{Position = new Position{X = position.X + 1, Y = position.Y}, SharedWall = WallState.RIGHT});
+            }
+        }
+
+        return neighbors;
     }
 
     public static WallState[,] Generate(int width, int height)
@@ -31,6 +124,6 @@ public class MazeGenerator : MonoBehaviour
                 maze[i,j] = initial;
             }
         }
-        return maze;
+        return DFS(maze, width, height);
     }
 }
--- Assets/Scripts/MazePrinter.cs
+++ Assets/Scripts/MazePrinter.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class MazePrinter : MonoBehaviour
 {
-    public int width = 10;
-    public int height = 10;
+    public int width = 5;
+    public int height = 5;
     public float size = 1f;
     public Transform wallPrefab = null;
 
     void Start()
     {
-        var maze = MazeGenerator.Generate(width, height);
+        MazeGenerator.WallState[,] maze = MazeGenerator.Generate(width, height);
         Draw(maze);
     }
 
@@ -24,18 +24,18 @@ public class MazePrinter : MonoBehaviour
         {
             for (int j = 0; j < height; j++)
             {
-                var cell = maze[i,j];
-                var position = new Vector3(-width/2 + i, -height/2 + j, 0);
+                MazeGenerator.WallState cell = maze[i,j];
+                Vector3 position = new Vector3(-width/2 + i, -height/2 + j, 0);
 
                 if (cell.HasFlag(MazeGenerator.WallState.UP))
                 {
-                    var topWall = Instantiate(wallPrefab, transform) as Transform;
+                    Transform topWall = Instantiate(wallPrefab, transform);
                     topWall.position = position + new Vector3(0, size/2, 0);
                     topWall.localScale = new Vector3(size, topWall.localScale.x, topWall.localScale.y);
                 }
                 if (cell.HasFlag(MazeGenerator.WallState.LEFT))
                 {
-                    var leftWall = Instantiate(wallPrefab, transform) as Transform;
+                    Transform leftWall = Instantiate(wallPrefab, transform);
                     leftWall.position = position + new Vector3(-size/2, 0, 0);
                     leftWall.localScale = new Vector3(size, leftWall.localScale.x, leftWall.localScale.y);
                     leftWall.eulerAngles = new Vector3(0, 0, 90);
@@ -45,7 +45,7 @@ public class MazePrinter : MonoBehaviour
                 {
                     if (cell.HasFlag(MazeGenerator.WallState.RIGHT))
                     {
-                        var rightWall = Instantiate(wallPrefab, transform) as Transform;
+                        Transform rightWall = Instantiate(wallPrefab, transform);
                         rightWall.position = position + new Vector3(size/2, 0, 0);
                         rightWall.localScale = new Vector3(size, rightWall.localScale.x, rightWall.localScale.y);
                         rightWall.eulerAngles = new Vector3(0, 0, 90);
@@ -55,7 +55,7 @@ public class MazePrinter : MonoBehaviour
                 {
                     if (cell.HasFlag(MazeGenerator.WallState.DOWN))
                     {
-                        var bottomWall = Instantiate(wallPrefab, transform) as Transform;
+                        Transform bottomWall = Instantiate(wallPrefab, transform);
                         bottomWall.position = position + new Vector3(0, -size/2, 0);
                         bottomWall.localScale = new Vector3(size, bottomWall.localScale.x, bottomWall.localScale.y);
                     }
