5048d7d9102cdc5d19a35efee2129d7ccb5cf064
Matt Umut Ucak <umutucak79@gmail.com>
2021-11-25 13:04:02 +0100

    Issue #6 solved

--- Assets/Scripts/MazePrinter.cs
+++ Assets/Scripts/MazePrinter.cs
@@ -8,6 +8,7 @@ public class MazePrinter : MonoBehaviour
     [SerializeField] private int width;
     [SerializeField] private int height;
     [SerializeField] private float size;
+    private readonly float wallThickness = 0.04493808f;
     [SerializeField] private Transform wallPrefab;
 
     void Start()
@@ -25,42 +26,52 @@ public class MazePrinter : MonoBehaviour
             for (int j = 0; j < height; j++)
             {
                 MazeGenerator.Cell cell = maze[i,j];
-                Vector3 position = new Vector3(-width/2 + i, -height/2 + j, 0);
+                Vector3 position = new Vector3(i*size - width*size/2 + size/2, j*size - height*size/2 + size/2, 0);
 
                 if (cell.HasFlag(MazeGenerator.Cell.UP))
                 {
                     Transform topWall = Instantiate(wallPrefab, transform);  //place wall on the game scene
                     topWall.position = position + new Vector3(0, size/2, 0); //choose the location of the wall
-                    topWall.localScale = new Vector3(size, topWall.localScale.x, topWall.localScale.y); //the size of the wall
+                    topWall.localScale = new Vector3(wallThickness, size, 0); //the size of the wall
+                    topWall.eulerAngles = new Vector3(0, 0, 90); //flip the horizontal walls
                 }
                 if (cell.HasFlag(MazeGenerator.Cell.LEFT))
                 {
                     Transform leftWall = Instantiate(wallPrefab, transform); //place wall on the game scene
                     leftWall.position = position + new Vector3(-size/2, 0, 0); //choose the location of the wall
-                    leftWall.localScale = new Vector3(size, leftWall.localScale.x, leftWall.localScale.y); //the size of the wall
-                    leftWall.eulerAngles = new Vector3(0, 0, 90); //flip the horizontal walls
+                    leftWall.localScale = new Vector3(wallThickness, size, 0); //the size of the wall
                 }
 
-                if (i == width - 1) //for the very far right of the grid
+                if (i == width - 1) //for the very far right of the grid, so that there arent multiple walls when only 1 is needed.
                 {
                     if (cell.HasFlag(MazeGenerator.Cell.RIGHT))
                     {
                         Transform rightWall = Instantiate(wallPrefab, transform); //place wall on the game scene
                         rightWall.position = position + new Vector3(size/2, 0, 0); //choose the location of the wall
-                        rightWall.localScale = new Vector3(size, rightWall.localScale.x, rightWall.localScale.y); //the size of the wall
-                        rightWall.eulerAngles = new Vector3(0, 0, 90); //flip the horizontal walls
+                        rightWall.localScale = new Vector3(wallThickness, size, 0); //the size of the wall
                     }
                 }
                 if (j == 0)
                 {
-                    if (cell.HasFlag(MazeGenerator.Cell.DOWN)) //for the very bottom of the grid
+                    if (cell.HasFlag(MazeGenerator.Cell.DOWN)) //for the very bottom of the grid, so that there arent multiple walls when only 1 is needed.
                     {
                         Transform bottomWall = Instantiate(wallPrefab, transform); //place wall on the game scene
                         bottomWall.position = position + new Vector3(0, -size/2, 0); //choose the location of the wall
-                        bottomWall.localScale = new Vector3(size, bottomWall.localScale.x, bottomWall.localScale.y); //the size of the wall
+                        bottomWall.localScale = new Vector3(wallThickness, size, 0); //the size of the wall
+                        bottomWall.eulerAngles = new Vector3(0, 0, 90); //flip the horizontal walls
                     }
                 }
             }
         }
+    }
+    
+    public int GetMazeWidth()
+    {
+        return width;
+    }
+
+    public int GetMazeHeight()
+    {
+        return height;
     }
 }
